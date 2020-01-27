d78a5bf8f3268b09cc2eaee71aee442a2adf5c16
agrensimon <simon.agren@gmail.com>
2020-01-27 16:02:29 +0100

    charuco cam calibration test

---
+++ charuco_cam-calibration.py
@@ -0,0 +1,26 @@
+import numpy as np
+import cv2, PIL, os
+from cv2 import aruco
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import pandas as pd
+#matplotlib nbagg
+
+workdir = "./workdir/"
+aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+board = aruco.CharucoBoard_create(7, 5, 1, .8, aruco_dict)
+imboard = board.draw((2000, 2000))
+cv2.imwrite(workdir + "chessboard.tiff", imboard)
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+plt.imshow(imboard, cmap = mpl.cm.gray, interpolation = "nearest")
+ax.axis("off")
+plt.show()
+
+datadir = "../../data/calib_tel_ludo/"
+images = np.array([datadir + f for f in os.listdir(datadir) if f.endswith(".png") ])
+order = np.argsort([int(p.split(".")[-2].split("_")[-1]) for p in images])
+images = images[order]
+images
+
