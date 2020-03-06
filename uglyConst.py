38f019ce528e165d3daa345b9e0124360777e452
agrensimon <simon.agren@gmail.com>
2020-03-06 09:25:17 +0100

    name change of constants file

---
+++ uglyConst.py
@@ -0,0 +1,36 @@
+#-- 
+
+#-- OpenCV settings
+CAM_NR = 0
+MARKER_OFFSET = 0.08935
+MARKERSIZE_SMALL = 0.0215
+MARKERSIZE_BIG = 0.112
+
+#-- OpenCV colors
+BLUE = (255,0,0)
+GREEN = (0,255,0)
+RED = (0,0,255)
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+
+#-- State machine
+TAKEOFF_HEIGHT = 0.5    
+DIST_IGE = 0.2          # [m]
+DIST_IGE_HYST = 0.01
+R_CYLINDER = 0.05       # radius of x,y [m]
+R_CYLINDER_HYST = 0.04     
+
+#-- Control parameters
+Kyaw = 1.0
+Kx = 0.2
+Ky = 0.2
+Kz = 40
+
+velKp = 25.0
+velKi = 15.0
+velKd = 0.0
+
+MAX_XVEL = 1.0
+MAX_YVEL = 1.0
+MAX_ZVEL = 1.0
+
